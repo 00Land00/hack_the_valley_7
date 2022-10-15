f986914f8bf8458ae0ef760706e972a15a398094
Lander Joshua Vitug <lander.vitug@gmail.com>
2022-10-15 12:41:14 -0400

    create schema and connection to database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,46 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const progressSchema = new mongoose.Schema({
+  name: String,
+  value: Number,
+  level: Number,
+  max_val: Number
+})
+
+const exerciseSchema = new mongoose.Schema({
+  name: String,
+  categories: [String],
+  day_of_week: String
+})
+
+const Progress = mongoose.model('Progress', progressSchema)
+const Exercise = mongoose.model('Exercise', exerciseSchema)
+console.log(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI).then((result) => {
+  console.log('connected')
+
+  const progress = new Progress({
+    name: "upper body",
+    value: 34,
+    level: 2,
+    max_val: 100
+  })
+
+  const exercise = new Exercise({
+    name: "Bench Press",
+    categories: ['Upper Body'],
+    day_of_week: 'Tuesday'
+  })
+
+  progress.save()
+  exercise.save()
+}).then(() => {
+  console.log('progress saved!')
+  return mongoose.connection.close()
+}).catch((err) => console.log(err))
 
 let notes = [
   "test1", "test2", "test3"
